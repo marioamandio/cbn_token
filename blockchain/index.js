9caca67ca7b980bcf4f34ff1d07fdbe2466d7696
Mario Amandio <marioamandio@gmail.com>
2021-02-07 19:28:59 +0000

    cabanoes token

---
+++ blockchain/index.js
@@ -0,0 +1,63 @@
+const Block = require("./block");
+const { cryptoHash } = require("../util");
+class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
+
+  static isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, lastHash, hash, data, difficulty, nonce } = chain[i];
+
+      const actualLastHash = chain[i - 1].hash;
+      const lastDifficulty = chain[i - 1].difficulty;
+
+      if (lastHash !== actualLastHash) return false;
+
+      const validatedHash = cryptoHash(
+        timestamp,
+        lastHash,
+        data,
+        difficulty,
+        nonce
+      );
+
+      if (hash !== validatedHash) return false;
+
+      if (Math.abs(lastDifficulty - difficulty) > 1) return false;
+    }
+
+    return true;
+  }
+
+  addBlock({ data }) {
+    const newBlock = Block.mineBlock({
+      lastBlock: this.chain[this.chain.length - 1],
+      data,
+    });
+
+    this.chain.push(newBlock);
+  }
+
+  replaceChain(chain) {
+    if (chain.length <= this.chain.length) {
+      console.error("the incoming chain must be longer");
+      return;
+    }
+
+    if (!Blockchain.isValidChain(chain)) {
+      console.error("the incoming chain must be valid");
+
+      return;
+    }
+    console.log("the incoming chain will change");
+
+    this.chain = chain;
+  }
+}
+
+module.exports = Blockchain;
---
+++ scripts/average-work.js
@@ -0,0 +1,29 @@
+const Blockchain = require("./");
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock({ data: "initial" });
+// console.log("first block", blockchain.chain[blockchain.chain.length - 1]);
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
+
+const times = [];
+
+for (let i = 0; i < 1000; i++) {
+  prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
+
+  blockchain.addBlock({ data: `block ${i}` });
+
+  nextBlock = blockchain.chain[blockchain.chain.length - 1];
+
+  nextTimestamp = nextBlock.timestamp;
+
+  timeDiff = nextTimestamp - prevTimestamp;
+
+  times.push(timeDiff);
+
+  average = times.reduce((total, num) => total + num, 0) / times.length;
+
+  console.log(
+    `time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`
+  );
+}
